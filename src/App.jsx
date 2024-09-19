263b63dfe84dea5510ac46bcc916b81a3179c504
gramatchi <nik98.champion@gmail.com>
2024-09-19 16:58:25 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import './App.css'
-import Catalog from './pages/CatalogPage/Catalog'
-import Favotities from './pages/FavoritesPage/Favotities'
-import Home from './pages/HomePage/Home'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Catalog from "./pages/CatalogPage/Catalog";
+import Favotities from "./pages/FavoritesPage/Favotities";
+import Home from "./pages/HomePage/Home";
 
 function App() {
-
-
   return (
     <>
-      <Home/>
-      <Catalog/>
-      <Favotities/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favotities />} />
+        <Route path="/Catalog" element={<Catalog />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
